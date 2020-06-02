640e8a1c6d4774b07ea9799b62edb8ba75e4833f
hi <hi@DESKTOP-G463TQN>
2020-06-02 18:03:20 +0900

    ex01

---
+++ src/com/javaex/ex01/Person.java
@@ -0,0 +1,56 @@
+package com.javaex.ex01;
+
+public class Person {
+	
+	private String name;
+	private String hp;
+	private String company;
+	
+	public Person() {}
+	
+	public Person(String name, String hp, String company) {
+		this.name = name;
+		this.hp = hp;
+		this.company = company;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getHp() {
+		return hp;
+	}
+
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	
+	public void showList() {
+		System.out.println("   " + name + "       " + hp + "     " + company);
+	}
+	
+	public void searchList() {
+		
+	}
+
+	public String DBinfo() {
+		String info = name + "," + hp + "," + company;
+		return info;
+	}
+	
+	
+
+}
---
+++ src/com/javaex/ex01/PhoneApp.java
@@ -0,0 +1,132 @@
+package com.javaex.ex01;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PhoneApp {
+
+    public static void main(String[] args) throws IOException {
+    	
+    	Reader fr = new FileReader("PhoneDB.txt");
+    	BufferedReader br = new BufferedReader(fr);
+		
+    	List<Person> pList = new ArrayList<Person>();
+    	
+    	while(true) {
+    		String str = br.readLine();
+    		if(str == null) {
+    			break;
+    		}
+    		String[] info = str.split(",");
+    		String name = info[0];
+			String hp = info[1];
+			String company = info[2];
+			pList.add(new Person(info[0], info[1], info[2]));
+			System.out.println("이름:" + name);
+			System.out.println("휴대폰:" + hp);
+			System.out.println("회사:" + company);
+			System.out.println("");
+    		
+    	}
+    	
+    	Writer fw = new FileWriter("PhoneDB.txt");
+		BufferedWriter bw = new BufferedWriter(fw);
+    	
+    	Scanner menusc = new Scanner(System.in);
+    	Scanner input = new Scanner(System.in);
+    	Scanner del = new Scanner(System.in);
+    	Scanner search = new Scanner(System.in);
+    	int menu;
+    	boolean menuflag = true;
+
+    	System.out.println("*******************************************");
+    	System.out.println("*             전화번호 관리 프로그램                      *");
+    	System.out.println("*******************************************");
+    	
+    	
+    	try {
+   
+        	while(menuflag) {
+           		System.out.println("1.리스트        2.등록       3.삭제       4.검색        5.종료");
+               	System.out.println("-------------------------------------------");
+               	System.out.print(">메뉴번호:");
+           		menu = menusc.nextInt();
+           		switch(menu) {
+               	case 1:
+               		System.out.println("<1.리스트>");
+               		for(int i=0;i<pList.size();i++) {
+                		System.out.print(i+1 + ".");
+               			pList.get(i).showList();
+               		}
+               		break;
+               	case 2:
+               		System.out.println("<2.등록>");
+               		System.out.print(">이름: ");
+               		String name = input.nextLine();
+               		System.out.print(">휴대전화: ");
+               		String hp = input.nextLine();
+               		System.out.print(">회사전화: ");
+               		String company = input.nextLine();
+               		pList.add(new Person(name, hp, company));
+               		System.out.println("[등록되었습니다.]");
+               		break;
+               	case 3:
+               		System.out.println("<3.삭제>");
+               		System.out.print(">번호: ");
+               		int delList = del.nextInt();
+                	pList.remove(delList);
+               		break;
+               	case 4:
+               		System.out.println("<4.검색>");
+               		System.out.print(">이름: ");
+               		String srch = search.nextLine();
+               		for(int i=0;i<pList.size();i++) {
+               			if((pList.get(i).getName()).contains(srch)) {
+               				pList.get(i).showList();
+               			}
+               		}
+               		break;
+                case 5:
+                   	menuflag = false;
+               		break;
+               	default:
+               		System.out.println("[다시 입력해 주세요.]");
+               		break;
+               	}
+                     	
+           	}
+    	} catch(Exception e) {
+    		System.out.println("에러");
+    	}
+    	
+    	
+   	
+   		for(int i=0;i<pList.size();i++) {
+   			bw.write(pList.get(i).DBinfo());
+   			bw.newLine();
+   			}
+    	
+    	
+    	System.out.println("*******************************************");
+    	System.out.println("*                  감사합니다                            *");
+    	System.out.println("*******************************************");
+
+    	
+    	menusc.close();
+    	input.close();
+    	del.close();
+    	search.close();
+    	br.close();
+    	bw.close();
+    	
+    }
+
+}
